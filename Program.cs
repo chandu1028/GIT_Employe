c7f53e579e4a99bd32ddffa9c961ae7de2e26353
chandu1028 <144781340+chandu1028@users.noreply.github.com>
2024-07-01 10:50:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+//using EMPLOYEE.Models;
+using EMPLOYEEDB.Models;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+IConfiguration configuration;
+
+configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+builder.Services.AddDbContext<MyDbContext>
+
+    (option => option.UseSqlServer(configuration.GetConnectionString("EmployeeConnection")));
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
